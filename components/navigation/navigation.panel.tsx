4c36a4fe18b1c6f794fe82d90f53a84e0b862622
voronov <voronov54@gmail.com>
2022-09-16 18:56:41 +0700

    code cleaning

--- components/navigation/navigation.panel.tsx
+++ components/navigation/navigation.panel.tsx
@@ -2,8 +2,6 @@ import * as React from "react"
 import Link from "next/link"
 import faviconPic from '../../public/favicon.ico'
 import Image from "next/image"
-import { API_CLIENT } from "../../services/api/api-client"
-import { API_ERROR_HANDLER } from "../../services/api/api-error-handler"
 import { USERS_SERVICE } from "../../services/users/users.service"
 import NotAuthorizedUserButton from "../buttons/not.authorized.user.button"
 import AuthorizedUserButton from "../buttons/authorized.user.button"
@@ -11,25 +9,6 @@ import { useProfile } from '../hooks/use.profile.hook'
 
 const NavigationPanel = () => {
     const [profile, setProfile] = useProfile()
-
-    const ping = () => {
-        API_CLIENT.auth.ping()
-            .then((response) => {
-                console.log(response)
-            })
-    }
-
-    const safePing = () => {
-        API_CLIENT.auth.safePing()
-            .then((response) => {
-                console.log(response)
-            })
-
-        API_ERROR_HANDLER.callWithErrorHandling({
-            action: () => API_CLIENT.auth.safePing(),
-        })
-
-    }
 
     React.useEffect(() => {
         USERS_SERVICE.getMe().then((res) => {
@@ -50,16 +29,6 @@ const NavigationPanel = () => {
                 </Link>
             </div>
             <div className="flex items-center">
-                <Link href="">
-                    <a onClick={ping} className="text-base font-medium text-gray-500 hover:text-gray-900 flex items-center ml-5">
-                        Ping
-                    </a>
-                </Link>
-                <Link href="">
-                    <a onClick={safePing} className="text-base font-medium text-gray-500 hover:text-gray-900 flex items-center ml-5">
-                        Safe Ping
-                    </a>
-                </Link>
                 <Link href="/">
                     <a className="text-base font-medium text-gray-500 hover:text-gray-900 flex items-center ml-5">
                         Posts
