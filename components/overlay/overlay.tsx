6c9dd1c71e01951ccd0d9951c9a2e56bfa1bc968
voronov <voronov54@gmail.com>
2022-11-05 18:57:47 +0700

    refactoring of styles

--- components/overlay/overlay.tsx
+++ components/overlay/overlay.tsx
@@ -7,9 +7,9 @@ const Overlay = () => {
 
     return (
         <div className="h-screen flex flex-col items-center justify-center">
-            <div className={styles.loader + " ease-linear rounded-full border-4 border-t-4 border-white h-14 w-14 mb-4"}></div>
-            <h2 className="text-center text-xl font-semibold">{t("overlay.header")}...</h2>
-            <p className="w-1/3 text-center ">{t("overlay.subheader")}</p>
+            <div className={styles.loader + " primary-loader ease-linear rounded-full border-4 border-t-4 h-14 w-14 mb-4"} />
+            <h2 className="text-center text-xl font-semibold primary-loader-text">{t("overlay.header")}...</h2>
+            <p className="w-1/3 text-center primary-loader-text">{t("overlay.subheader")}</p>
         </div>
     )
 }
