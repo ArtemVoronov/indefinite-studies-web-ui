35109326dce8e2a4586e99afaa72c36a6e6bb1eb
voronov <voronov54@gmail.com>
2022-11-08 23:10:06 +0700

    fix some styles

--- components/posts/preview/posts.preview.tsx
+++ components/posts/preview/posts.preview.tsx
@@ -42,7 +42,7 @@ const PostPreview = (props: { post: FeedBlock }) => {
                 <div className="flex items-center text-xs">
                     {Tags.map(function (tag: Tag, idx) {
                         return (
-                            <StyledLink href={"/posts/" + tag.Id + "/0"} text={tag.Name} key={idx} />
+                            <StyledLink href={"/posts/" + tag.Id + "/0"} text={tag.Name} key={idx} classes="ml-2" />
                         )
                     })}
                 </div>
