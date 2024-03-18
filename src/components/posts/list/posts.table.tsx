c54b83c7463477551e04cfea8843c91049d56628
voronov <voronov54@gmail.com>
2024-03-18 10:14:23 +0700

    code cleaning (indentation), fix for missed tags data

--- src/components/posts/list/posts.table.tsx
+++ src/components/posts/list/posts.table.tsx
@@ -10,92 +10,92 @@ import StyledLinkButton from "../../buttons/styled.link.button"
 const DEFAULT_MAX_POSTS_PER_PAGE = 5
 
 const PostsTable = (props: { tagId: string, page: string, postState: string, userUuid: string, withModeratorActions?: boolean, pageSize?: number, onNavigate?: (page: number) => void }) => {
-    const [isLoading, setIsLoading] = React.useState(false)
-    const [posts, setPosts] = React.useState([])
-    const [loadedCount, setLoadedCount] = React.useState(0)
-    const { t } = useTranslation()
-    const MAX_POSTS_PER_PAGE = props.pageSize ? props.pageSize : DEFAULT_MAX_POSTS_PER_PAGE
-    const LIMIT = MAX_POSTS_PER_PAGE + 1
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [posts, setPosts] = React.useState([])
+  const [loadedCount, setLoadedCount] = React.useState(0)
+  const { t } = useTranslation()
+  const MAX_POSTS_PER_PAGE = props.pageSize ? props.pageSize : DEFAULT_MAX_POSTS_PER_PAGE
+  const LIMIT = MAX_POSTS_PER_PAGE + 1
 
-    const fetchPosts = async () => {
-        const timer = setTimeout(() => {
-            setIsLoading(true)
-        }, SPIN_ICON_SHOWING_TIMEOUT)
+  const fetchPosts = async () => {
+    const timer = setTimeout(() => {
+      setIsLoading(true)
+    }, SPIN_ICON_SHOWING_TIMEOUT)
 
-        try {
-            const response = await FEED_SERVICE.getAll({ offset: parseInt(props.page) * MAX_POSTS_PER_PAGE, limit: LIMIT, tagId: props.tagId, state: props.postState, userUuid: props.userUuid })
-            clearTimeout(timer)
-            if (response.status === 200) {
-                const portion = response.data.Data
-                const count = response.data.Count
-                setLoadedCount(count)
-                if (count > MAX_POSTS_PER_PAGE) {
-                    setPosts(portion.slice(0, MAX_POSTS_PER_PAGE))
-                } else {
-                    setPosts(portion)
-                }
-            }
-        } finally {
-            clearTimeout(timer)
-            setIsLoading(false)
+    try {
+      const response = await FEED_SERVICE.getAll({ offset: parseInt(props.page) * MAX_POSTS_PER_PAGE, limit: LIMIT, tagId: props.tagId, state: props.postState, userUuid: props.userUuid })
+      clearTimeout(timer)
+      if (response.status === 200) {
+        const portion = response.data.Data
+        const count = response.data.Count
+        setLoadedCount(count)
+        if (count > MAX_POSTS_PER_PAGE) {
+          setPosts(portion.slice(0, MAX_POSTS_PER_PAGE))
+        } else {
+          setPosts(portion)
         }
+      }
+    } finally {
+      clearTimeout(timer)
+      setIsLoading(false)
     }
+  }
 
-    React.useEffect(() => {
-        fetchPosts()
-    }, [props.page, props.tagId, props.postState, props.pageSize])
+  React.useEffect(() => {
+    fetchPosts()
+  }, [props.page, props.tagId, props.postState, props.pageSize])
 
-    const navigation = (
-        <div className="flex justify-center p-0 my-0 border-b-2 primary-content-block"
-            style={{ display: (posts.length + 1) != loadedCount && props.page == "0" ? "none" : undefined }}>
-            <StyledLinkButton
-                text={t("btn.prev")} icon={<ArrowLeftIcon />}
-                onClick={() => { props.onNavigate ? props.onNavigate(parseInt(props.page) - 1) : "" }}
-                style={{ display: props.page == "0" ? "none" : undefined }}
-            />
-            <div className="flex-1" />
-            <StyledLinkButton
-                text={t("btn.next")} icon={<ArrowRightIcon />}
-                onClick={() => { props.onNavigate ? props.onNavigate(parseInt(props.page) + 1) : "" }}
-                style={{ display: (posts.length + 1) != loadedCount ? "none" : undefined }}
-            />
-        </div>
-    )
+  const navigation = (
+    <div className="flex justify-center p-0 my-0 border-b-2 primary-content-block"
+      style={{ display: (posts.length + 1) != loadedCount && props.page == "0" ? "none" : undefined }}>
+      <StyledLinkButton
+        text={t("btn.prev")} icon={<ArrowLeftIcon />}
+        onClick={() => { props.onNavigate ? props.onNavigate(parseInt(props.page) - 1) : "" }}
+        style={{ display: props.page == "0" ? "none" : undefined }}
+      />
+      <div className="flex-1" />
+      <StyledLinkButton
+        text={t("btn.next")} icon={<ArrowRightIcon />}
+        onClick={() => { props.onNavigate ? props.onNavigate(parseInt(props.page) + 1) : "" }}
+        style={{ display: (posts.length + 1) != loadedCount ? "none" : undefined }}
+      />
+    </div>
+  )
 
 
-    if (isLoading) return (
-        <div>
-            <Overlay />
-        </div>
-    )
+  if (isLoading) return (
+    <div>
+      <Overlay />
+    </div>
+  )
 
-    if (posts.length == 0) return (
-        <div>
-            {t("no.data")}
-        </div>
-    )
+  if (posts.length == 0) return (
+    <div>
+      {t("no.data")}
+    </div>
+  )
 
-    return (
-        <div className="flex flex-1 flex-col">
-            {navigation}
-            <table className="table-auto flex-1">
-                <thead>
-                    <tr className="primary-content-block">
-                        <th>{t("posts.page.table.head.topic")}</th>
-                        {props.withModeratorActions && (<th className="w-64">{t("posts.page.table.head.author")}</th>)}
-                        <th className="w-64">{t("posts.page.table.head.action")}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {posts.map(function (p: FeedBlock, idx) {
-                        return (
-                            <PostRow key={idx} post={p} withModeratorActions={props.withModeratorActions} />
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
+  return (
+    <div className="flex flex-1 flex-col">
+      {navigation}
+      <table className="table-auto flex-1">
+        <thead>
+          <tr className="primary-content-block">
+            <th>{t("posts.page.table.head.topic")}</th>
+            {props.withModeratorActions && (<th className="w-64">{t("posts.page.table.head.author")}</th>)}
+            <th className="w-64">{t("posts.page.table.head.action")}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map(function (p: FeedBlock, idx) {
+            return (
+              <PostRow key={idx} post={p} withModeratorActions={props.withModeratorActions} />
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default PostsTable
--- src/components/tags/tags.list.tsx
+++ src/components/tags/tags.list.tsx
@@ -5,70 +5,73 @@ import { DEFAULT_LIMIT, SPIN_ICON_SHOWING_TIMEOUT } from "../../utils/utils"
 import { useTranslation } from "gatsby-plugin-react-i18next"
 
 const TagsList = () => {
-    const [isLoading, setIsLoading] = React.useState(false)
-    const [tags, setTags] = React.useState([])
-    const [isAllFetched, setIsAllFetched] = React.useState(false)
-    const [offset, setOffset] = React.useState(0)
-    const { t } = useTranslation()
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [tags, setTags] = React.useState([])
+  const [isAllFetched, setIsAllFetched] = React.useState(false)
+  const [offset, setOffset] = React.useState(0)
+  const { t } = useTranslation()
 
-    const fetchTags = async () => {
-        if (isAllFetched) {
-            return
-        }
-        const timer = setTimeout(() => {
-            setIsLoading(true)
-        }, SPIN_ICON_SHOWING_TIMEOUT)
-
-        try {
-            const response = await POSTS_SERVICE.getTags({ offset, limit: DEFAULT_LIMIT })
-            clearTimeout(timer)
-            if (response.status === 200) {
-                const count = response.data.Count
-                const portion = response.data.Data
-                if (count != 0) {
-                    setTags(tags.concat(portion))
-                }
-                if (portion.length == DEFAULT_LIMIT) {
-                    setOffset(offset + DEFAULT_LIMIT)
-                }
-                if (portion.length < DEFAULT_LIMIT) {
-                    setIsAllFetched(true)
-                }
-            }
-        } finally {
-            clearTimeout(timer)
-            setIsLoading(false)
-        }
+  const fetchTags = async () => {
+    if (isAllFetched) {
+      return
     }
+    const timer = setTimeout(() => {
+      setIsLoading(true)
+    }, SPIN_ICON_SHOWING_TIMEOUT)
 
-    React.useEffect(() => {
-        fetchTags()
-    }, [offset])
+    try {
+      const response = await POSTS_SERVICE.getTags({ offset, limit: DEFAULT_LIMIT })
+      clearTimeout(timer)
+      if (response.status === 200) {
+        const count = response.data.Count
+        const portion = response.data.Data
+        if (!portion) {
+          return
+        }
+        if (count != 0) {
+          setTags(tags.concat(portion))
+        }
+        if (portion.length == DEFAULT_LIMIT) {
+          setOffset(offset + DEFAULT_LIMIT)
+        }
+        if (portion.length < DEFAULT_LIMIT) {
+          setIsAllFetched(true)
+        }
+      }
+    } finally {
+      clearTimeout(timer)
+      setIsLoading(false)
+    }
+  }
+
+  React.useEffect(() => {
+    fetchTags()
+  }, [offset])
 
 
-    if (isLoading) return (
-        <div>
-            <Overlay />
-        </div>
-    )
+  if (isLoading) return (
+    <div>
+      <Overlay />
+    </div>
+  )
 
-    if (tags.length == 0) return (
-        <div>
-            {t("no.data")}
-        </div>
-    )
+  if (tags.length == 0) return (
+    <div>
+      {t("no.data")}
+    </div>
+  )
 
-    return (
-        <div className="w-full max-w-3xl">
-            <div className="flex">
-                {tags.map(function (p: Tag, idx) {
-                    return (
-                        <div className="primary-button p-1 m-1" key={idx}>{p.Name}</div>
-                    )
-                })}
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full max-w-3xl">
+      <div className="flex">
+        {tags.map(function (p: Tag, idx) {
+          return (
+            <div className="primary-button p-1 m-1" key={idx}>{p.Name}</div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default TagsList
